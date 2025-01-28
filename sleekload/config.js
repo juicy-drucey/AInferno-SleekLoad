e817cf1476be828b63b312d9f27b9b25cb713766
juicy-drucey <alexdruce2@gmail.com>
2025-01-28 16:55:37 +0800

    Update config.js

--- sleekload/config.js
+++ sleekload/config.js
@@ -32,11 +32,11 @@ config.logo_servername = false
 
 // Center logo?
 // "true" or "false"
-config.logo_center = false
+config.logo_center = true
 
 // Display current map and gamemode?
 // "true" or "false"
-config.logo_show_info = true
+config.logo_show_info = false
 
 // Use a video for the background?
 // Otherwise it will use images
